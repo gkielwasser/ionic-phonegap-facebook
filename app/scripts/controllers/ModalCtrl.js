92e82a6c225e2d214d4a3b80e82bc943146df65c
Gab <gab@a.fr>
2014-03-27 18:27:36 +0100

    update gitignore

---
+++ app/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('starter.controllers')
+
+.controller('ModalCtrl', ['$scope','$ionicModal',function($scope, $ionicModal) {
+  $scope.$watch("getSelectedFriend()",function(value){
+    if($scope.modal && value && value.length == 0){
+      $scope.closeModal();
+    }
+  },true);
+
+  // Load the modal from the given template URL
+  $ionicModal.fromTemplateUrl('views/friendsConfirmationModal.html', function(modal) {
+    $scope.modal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+}])
